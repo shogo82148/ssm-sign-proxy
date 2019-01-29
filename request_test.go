7b009a8f5fe7b91ac9314c095db8f6c5449c61c5
Ichinose Shogo <shogo82148@gmail.com>
2019-01-29 18:55:21 +0900

    add test for POST

--- request_test.go
+++ request_test.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -27,6 +28,36 @@ func TestRequestRequest(t *testing.T) {
 			},
 			IsBase64Encoded: false,
 			Body:            "",
+		}
+		if !reflect.DeepEqual(req, want) {
+			t.Errorf("want %#v, got %#v", want, req)
+		}
+	})
+
+	t.Run("post", func(t *testing.T) {
+		str := `{"hello":"world"}`
+		r := strings.NewReader(str)
+		httpreq := httptest.NewRequest(http.MethodPost, "http://example.com/foobar", r)
+		httpreq.Header.Set("Content-Type", "application/json; charset=utf-8")
+		req, err := NewRequest(httpreq)
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := &Request{
+			HTTPMethod:                      http.MethodPost,
+			Path:                            "/foobar",
+			QueryStringParameters:           map[string]string{},
+			MultiValueQueryStringParameters: map[string][]string{},
+			Headers: map[string]string{
+				"Host":         "example.com",
+				"Content-Type": "application/json; charset=utf-8",
+			},
+			MultiValueHeaders: map[string][]string{
+				"Host":         []string{"example.com"},
+				"Content-Type": []string{"application/json; charset=utf-8"},
+			},
+			IsBase64Encoded: false,
+			Body:            `{"hello":"world"}`,
 		}
 		if !reflect.DeepEqual(req, want) {
 			t.Errorf("want %#v, got %#v", want, req)
