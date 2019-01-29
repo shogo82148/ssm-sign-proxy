e3773dbebe326d33f32b0c0b23a825ea87354785
Ichinose Shogo <shogo82148@gmail.com>
2019-01-29 22:30:34 +0900

    add flags

--- cmd/ssm-sign-proxy/main.go
+++ cmd/ssm-sign-proxy/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	proxy "github.com/shogo82148/ssm-sign-proxy"
 )
+
+var functionName, address string
+
+func init() {
+	flag.StringVar(&functionName, "function-name", "", "aws lambda function name")
+	flag.StringVar(&address, "address", "localhost:8000", "address for listening")
+}
 
 func main() {
 	cfg, err := external.LoadDefaultAWSConfig()
@@ -15,8 +23,8 @@ func main() {
 	}
 	p := &proxy.Proxy{
 		Config:       cfg,
-		FunctionName: "proxy-test",
+		FunctionName: functionName,
 	}
 
-	http.ListenAndServe(":8000", p)
+	http.ListenAndServe(address, p)
 }
