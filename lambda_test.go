e809d033edccc19fa82e8d51c86c7f4d26d2989c
Ichinose Shogo <shogo82148@gmail.com>
2019-03-25 19:56:31 +0900

    add tests for basic auth

--- lambda_test.go
+++ lambda_test.go
@@ -69,6 +69,70 @@ func TestLambdaHandle(t *testing.T) {
 			t.Errorf("want %s, goit %s", "/development/"+u.Host, aws.StringValue(mock.input.Path))
 		}
 	})
+
+	t.Run("basic", func(t *testing.T) {
+		ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			username, password, ok := req.BasicAuth()
+			if !ok {
+				t.Error("Authorization not found")
+				http.Error(w, "NG", http.StatusUnauthorized)
+				return
+			}
+			if username != "chooblarin" {
+				t.Errorf("invalid username: got %s, want chooblarin", username)
+				http.Error(w, "NG", http.StatusForbidden)
+				return
+			}
+			if password != "very-secret" {
+				t.Errorf("invalid password: got %s, want very-secret", username)
+				http.Error(w, "NG", http.StatusForbidden)
+				return
+			}
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, "ok")
+		}))
+		defer ts.Close()
+
+		u, err := url.Parse(ts.URL)
+		if err != nil {
+			panic(err)
+		}
+		mock := &ssmMock{
+			output: &ssm.GetParametersByPathOutput{
+				Parameters: []ssm.Parameter{
+					{
+						Name:  aws.String("/development/" + u.Host + "/basic/username"),
+						Value: aws.String("chooblarin"),
+					},
+					{
+						Name:  aws.String("/development/" + u.Host + "/basic/password"),
+						Value: aws.String("very-secret"),
+					},
+				},
+			},
+		}
+		l := &Lambda{
+			Prefix: "development",
+			Client: ts.Client(),
+			svcssm: mock,
+		}
+		req := httptest.NewRequest(http.MethodGet, ts.URL, nil)
+		r, err := NewRequest(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		resp, err := l.Handle(context.Background(), r)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if resp.Body != "ok" {
+			t.Errorf("want %s, got %s", "ok", resp.Body)
+		}
+
+		if aws.StringValue(mock.input.Path) != "/development/"+u.Host {
+			t.Errorf("want %s, goit %s", "/development/"+u.Host, aws.StringValue(mock.input.Path))
+		}
+	})
 }
 
 func (mock *ssmMock) GetParametersByPathRequest(input *ssm.GetParametersByPathInput) ssm.GetParametersByPathRequest {
