63058e9e96c74216d9208c9fa97a0e9f8e186297
Ichinose Shogo <shogo82148@gmail.com>
2019-03-25 19:41:45 +0900

    refactor TestLambdaHandle

--- lambda_test.go
+++ lambda_test.go
@@ -15,67 +15,67 @@ import (
 
 type ssmMock struct {
 	ssmiface.SSMAPI
-	url   string
-	input *ssm.GetParametersByPathInput
+	input  *ssm.GetParametersByPathInput
+	output *ssm.GetParametersByPathOutput
 }
 
 func TestLambdaHandle(t *testing.T) {
-	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		secret := req.Header.Get("Secret-Key")
-		if secret != "very-secret" {
-			t.Errorf("want %s, got %s", "very secret", secret)
+	t.Run("headers", func(t *testing.T) {
+		ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			secret := req.Header.Get("Secret-Key")
+			if secret != "very-secret" {
+				t.Errorf("want %s, got %s", "very secret", secret)
+				http.Error(w, "NG", http.StatusForbidden)
+				return
+			}
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, "ok")
+		}))
+		defer ts.Close()
+
+		u, err := url.Parse(ts.URL)
+		if err != nil {
+			panic(err)
 		}
-		fmt.Fprint(w, "ok")
-	}))
-	defer ts.Close()
+		mock := &ssmMock{
+			output: &ssm.GetParametersByPathOutput{
+				Parameters: []ssm.Parameter{
+					{
+						Name:  aws.String("/development/" + u.Host + "/headers/secret-key"),
+						Value: aws.String("very-secret"),
+					},
+				},
+			},
+		}
+		l := &Lambda{
+			Prefix: "development",
+			Client: ts.Client(),
+			svcssm: mock,
+		}
+		req := httptest.NewRequest(http.MethodGet, ts.URL, nil)
+		r, err := NewRequest(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		resp, err := l.Handle(context.Background(), r)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if resp.Body != "ok" {
+			t.Errorf("want %s, got %s", "ok", resp.Body)
+		}
 
-	mock := &ssmMock{
-		url: ts.URL,
-	}
-	l := &Lambda{
-		Prefix: "development",
-		Client: ts.Client(),
-		svcssm: mock,
-	}
-	req := httptest.NewRequest(http.MethodGet, ts.URL, nil)
-	r, err := NewRequest(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	resp, err := l.Handle(context.Background(), r)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if resp.Body != "ok" {
-		t.Errorf("want %s, got %s", "ok", resp.Body)
-	}
-
-	u, err := url.Parse(mock.url)
-	if err != nil {
-		panic(err)
-	}
-	if aws.StringValue(mock.input.Path) != "/development/"+u.Host {
-		t.Errorf("want %s, goit %s", "/development/"+u.Host, aws.StringValue(mock.input.Path))
-	}
+		if aws.StringValue(mock.input.Path) != "/development/"+u.Host {
+			t.Errorf("want %s, goit %s", "/development/"+u.Host, aws.StringValue(mock.input.Path))
+		}
+	})
 }
 
 func (mock *ssmMock) GetParametersByPathRequest(input *ssm.GetParametersByPathInput) ssm.GetParametersByPathRequest {
 	mock.input = input
-	u, err := url.Parse(mock.url)
-	if err != nil {
-		panic(err)
-	}
-	out := &ssm.GetParametersByPathOutput{
-		Parameters: []ssm.Parameter{
-			{
-				Name:  aws.String("/development/" + u.Host + "/headers/secret-key"),
-				Value: aws.String("very-secret"),
-			},
-		},
-	}
 	return ssm.GetParametersByPathRequest{
 		Request: &aws.Request{
-			Data:        out,
+			Data:        mock.output,
 			HTTPRequest: &http.Request{},
 			Operation:   &aws.Operation{},
 		},
@@ -83,7 +83,7 @@ func (mock *ssmMock) GetParametersByPathRequest(input *ssm.GetParametersByPathIn
 		Copy: func(*ssm.GetParametersByPathInput) ssm.GetParametersByPathRequest {
 			return ssm.GetParametersByPathRequest{
 				Request: &aws.Request{
-					Data:        out,
+					Data:        mock.output,
 					HTTPRequest: &http.Request{},
 					Operation:   &aws.Operation{},
 				},
